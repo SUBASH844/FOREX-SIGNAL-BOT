b40fc7514d24bd01b9a00599e5a652b81a2ad718
SUBASH844 <xettri21967@gmail.com>
2025-05-16 20:56:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -37,5 +37,8 @@ def send_signal():
 def home():
     return "Bot is running"
 
+import os
+
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
